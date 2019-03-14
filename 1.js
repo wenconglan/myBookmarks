52ce8fa570c71589c9f4c41b229d31800fb1824b
logan <331089410@qq.com>
2019-03-14 19:01:41 +0800

    第一个js项目

---
+++ 1.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+    var length = array.length,
+        i,
+        j,
+        temp;
+    for (i = length - 1; 0 < i; i--) {
+        for (j = 0; j < i; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+ }
